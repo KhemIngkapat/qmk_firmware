65a105d634f96514d2c69a6b0318e6aa32dc04c7
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-07-18 23:23:56 +0700

    remove the tap dance remaining code, add numpad and oled layer display

--- keyboards/homie_board/keymaps/default/keymap.c
+++ keyboards/homie_board/keymaps/default/keymap.c
@@ -3,28 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-/* typedef enum { */
-/*     TD_NONE, */
-/*     TD_UNKNOW, */
-/*     TD_SINGLE_TAP, */
-/*     TD_SINGLE_HOLD, */
-/*     TD_DOUBLE_TAP, */
-/* } td_state_t; */
-/*  */
-/* typedef struct { */
-/*     bool is_press_action; */
-/*     td_state_t state; */
-/* } td_tap_t; */
-/*  */
-/* enum { */
-/*     A_SFCP */
-/* }; */
-/*  */
-/* td_state_t cur_dance(tap_dance_state_t *state); */
-/*  */
-/* void a_finished(tap_dance_state_t *state, void *user_data); */
-/* void a_reset(tap_dance_state_t *state, void *user_data); */
-
 #ifdef OLED_ENABLE
 
 // Rotate OLED
@@ -34,11 +12,27 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 // Draw to OLED
 bool oled_task_user() {
-    // Set cursor position
-    oled_set_cursor(0, 1);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Def\n"),false);
+            break;
+        case 1:
+            oled_write_P(PSTR("Acc\n"),false);
+            break;
+        case 2:
+            oled_write_P(PSTR("Fnc\n"),false);
+            break;
+        case 3:
+            oled_write_P(PSTR("Num\n"),false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Und"),false);
+    }
 
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.caps_lock ? PSTR("Caps") : PSTR("    "),false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "),false);
+
 
 
     return false;
@@ -51,6 +45,7 @@ const uint16_t PROGMEM combo_esc[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_bspc[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo_ent[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM combo_gui[] = {KC_W, KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo_caps[] = {KC_AT, KC_HASH, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_tab,KC_TAB),
@@ -58,23 +53,24 @@ combo_t key_combos[] = {
     COMBO(combo_bspc,KC_BSPC),
     COMBO(combo_ent,KC_ENT),
     COMBO(combo_gui,KC_LGUI),
+    COMBO(combo_caps,KC_CAPS),
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-KC_Q,         KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P,
-LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_SEMICOLON),
-LCTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B,        KC_N, KC_M, KC_COMMA, KC_DOT, RCTL_T(KC_SLASH),
-                          MO(2), KC_LALT,    KC_BSPC, LT(1,KC_SPC)
+KC_Q,         KC_W, KC_E, KC_R, KC_T,            KC_Y, KC_U, KC_I, KC_O, KC_P,
+LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G,            KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_SEMICOLON),
+LCTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMMA, KC_DOT, RCTL_T(KC_SLASH),
+                          MO(2), KC_LALT,        TG(3), LT(1,KC_SPC)
     ),
 
     [1] = LAYOUT(
 
 KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CAPS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                            KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS
     ),
 
@@ -84,43 +80,14 @@ KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,      KC_MINS, KC_EQL, KC_LPRN, KC_LBR
 KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT,
 KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_UNDS, KC_PLUS, KC_RPRN, KC_RBRC,  KC_RCBR,
                            KC_TRNS, KC_TRNS,    KC_BSLS, QK_REP
-    )
+    ),
+
+    [3] = LAYOUT(
+
+KC_NO, KC_1, KC_2, KC_3, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+KC_NO, KC_4, KC_5, KC_6, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+KC_NO, KC_7, KC_8, KC_9, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                   KC_TRNS, KC_0,     KC_TRNS, KC_NO
+    ),
 };
 
-/* td_state_t cur_dance(tap_dance_state_t *state) { */
-/*     if (state->count == 1) { */
-/*         if (state->interrupted || !state->pressed) return TD_SINGLE_TAP; */
-/*         else return TD_SINGLE_HOLD; */
-/*     } else if (state->count == 2) { */
-/*         return TD_DOUBLE_TAP; */
-/*     } else return TD_UNKNOW; */
-/* } */
-/*  */
-/* static td_tap_t atap_state = { */
-/*     .is_press_action = true, */
-/*     .state = TD_NONE */
-/* }; */
-/*  */
-/* void a_finished(tap_dance_state_t *state, void *user_data) { */
-/*     atap_state.state = cur_dance(state); */
-/*     switch (atap_state.state) { */
-/*         case TD_SINGLE_TAP: register_code(KC_A); break; */
-/*         case TD_SINGLE_HOLD: register_code(KC_LSFT); break; */
-/*         case TD_DOUBLE_TAP: register_code(KC_CAPS); break; */
-/*         default: break; */
-/*         } */
-/* } */
-/*  */
-/* void a_reset(tap_dance_state_t *state, void *user_data) { */
-/*     switch (atap_state.state) { */
-/*         case TD_SINGLE_TAP: unregister_code(KC_A); break; */
-/*         case TD_SINGLE_HOLD: unregister_code(KC_LSFT); break; */
-/*         case TD_DOUBLE_TAP: unregister_code(KC_CAPS); break; */
-/*         default: break; */
-/*     } */
-/*     atap_state.state = TD_NONE; */
-/* } */
-/*  */
-/* tap_dance_action_t tap_dance_actions[] = { */
-/*     [A_SFCP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, a_finished, a_reset) */
-/* }; */
